eda02d88f5e64e103f97b12cbd2d3efb72086ff9
Mikolaj Glinka <miki.glinka@gmail.com>
2026-01-27 14:58:00 +0100

    Create hello-types.ts
    day 2: add first typed functions in TypeScript

---
+++ typescript-basics/day-02/hello-types.ts
@@ -0,0 +1,8 @@
+const name: string = "Mikolaj";
+const year: number = 2026;
+
+function greet(user: string): string {
+  return `Hello ${user}, welcome to TypeScript`;
+}
+
+console.log(greet(name));
